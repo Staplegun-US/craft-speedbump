a315a2041403eb652518fdee90382a70d228c78b
Jack Bewley <jack@creativemethod.com>
2019-03-19 16:37:20 -0500

    Fix frontend AssetBundle so that it doesn't include cp resources

--- src/SpeedbumpAssets.php
+++ src/SpeedbumpAssets.php
@@ -2,7 +2,6 @@
 namespace staplegun\speedbump;
 
 use craft\web\AssetBundle;
-use craft\web\assets\cp\CpAsset;
 
 class SpeedbumpAssets extends AssetBundle
 {
@@ -10,11 +9,6 @@ class SpeedbumpAssets extends AssetBundle
     {
         // define the path that your publishable resources live
         $this->sourcePath = '@staplegun/speedbump/resources';
-
-        // define the dependencies
-        $this->depends = [
-            CpAsset::class,
-        ];
 
         // define the relative path to CSS/JS files that should be registered with the page
         // when this asset bundle is registered
